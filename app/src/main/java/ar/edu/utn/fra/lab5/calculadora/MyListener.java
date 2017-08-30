0eb068cc966fd9b8f9aa23af5997c5ad8141910e
lkdml <lazaro2002@hotmail.com>
2017-08-30 01:28:10 -0300

    Layout principal

---
+++ app/src/main/java/ar/edu/utn/fra/lab5/calculadora/MyListener.java
@@ -0,0 +1,20 @@
+package ar.edu.utn.fra.lab5.calculadora;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by lkdml on 30/08/2017.
+ */
+
+public class MyListener implements View.OnClickListener {
+
+
+    private Activity MyActivity;
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
