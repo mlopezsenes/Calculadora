4fba1b3ed780445629db69f23d3938e7ba9811c0
lkdml <lazaro2002@hotmail.com>
2017-08-31 02:49:10 -0300

    sin MVC

--- app/src/main/java/ar/edu/utn/fra/lab5/calculadora/MainActivity.java
+++ app/src/main/java/ar/edu/utn/fra/lab5/calculadora/MainActivity.java
@@ -1,13 +1,125 @@
 package ar.edu.utn.fra.lab5.calculadora;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Double ValorA;
+    private Double ValorB;
+    public double subTotal;
+    private MyListener listener;
+    private TextView txtSubTotal;
+    private TextView txtNumero;
+    private TextView txtOperador;
+    private String operacion;
+    private boolean esSubTotal;
+    private boolean esValorA;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.listener = new MyListener(this);
+
+        this.txtNumero = (TextView) findViewById(R.id.txtNumero);
+        this.txtSubTotal = (TextView) findViewById(R.id.txtSubTotal);
+        this.txtOperador = (TextView) findViewById(R.id.txtOperador);
+        this.limpiar();
+        // ConstraintLayout es un ViewGroup
+        ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.layout);
+        for (int i = 0; i < mainLayout.getChildCount(); i++) {
+            View view = mainLayout.getChildAt(i);
+            if (view instanceof Button) {
+                view.setOnClickListener(listener);
+            }
+
+        }
+
     }
+
+    public void limpiar(){
+        this.operacion="";
+        this.ValorA =0.0;
+        this.ValorB =0.0;
+        this.subTotal = 0;
+        this.txtNumero.setText("0");
+        this.txtSubTotal.setText("");
+        this.txtOperador.setText("");
+        this.esSubTotal=true;
+        this.esValorA=true;
+    }
+    public void agregarNumero(String texto){
+        if (!this.esSubTotal) {
+            String textoprevio = this.txtNumero.getText().toString();
+            if (textoprevio == "0") {
+                this.txtNumero.setText(texto);
+            } else {
+                this.txtNumero.setText(textoprevio + texto);
+            }
+        } else {
+            this.txtNumero.setText(texto);
+            this.esSubTotal=false;
+        }
+    }
+
+    public void agregarOperacion(String texto){
+        this.operacion=texto;
+        this.txtOperador.setText(this.operacion.toString());
+        this.txtSubTotal.setText(this.txtNumero.getText().toString());
+        this.esSubTotal=true;
+    }
+
+
+    public String calcular(Double ValorA, Double ValorB, String operacion){
+        String resultado = "0";
+            switch (this.operacion){
+                case "+":
+                    resultado = this.sumar(ValorA,ValorB);
+                    break;
+                case "-":
+                    resultado = this.restar(ValorA,ValorB);
+                    break;
+                case "*":
+                    resultado = this.multiplicar(ValorA,ValorB);
+                    break;
+                case "/":
+                    resultado = this.dividir(ValorA,ValorB);
+                    break;
+            }
+        return resultado;
+    }
+
+    public void procesar(){
+        this.txtNumero.setText(calcular(Double.valueOf(this.txtSubTotal.getText().toString()),Double.valueOf(this.txtNumero.getText().toString()),this.txtOperador.getText().toString()));
+       this.txtSubTotal.setText("");
+        this.txtOperador.setText("");
+    }
+    //Funciones matematicas
+    private String sumar(Double a, Double b){
+        return String.valueOf(a+b);
+    }
+    private String restar(Double a, Double b){
+        return String.valueOf(a-b);
+    }
+    private String dividir(Double a, Double b){
+        if (b>0){
+            return String.valueOf(a/b);
+        } else {
+            return "Error";
+        }
+    }
+    private String multiplicar(Double a, Double b){
+        return String.valueOf(a*b);
+    }
+
+
+
 }
--- app/src/main/java/ar/edu/utn/fra/lab5/calculadora/MyListener.java
+++ app/src/main/java/ar/edu/utn/fra/lab5/calculadora/MyListener.java
@@ -1,6 +1,5 @@
 package ar.edu.utn.fra.lab5.calculadora;
 
-import android.app.Activity;
 import android.view.View;
 
 /**
@@ -10,11 +9,66 @@ import android.view.View;
 public class MyListener implements View.OnClickListener {
 
 
-    private Activity MyActivity;
+    private MainActivity miActivity;
 
+    public MyListener(MainActivity miActivity) {
+        this.miActivity =  miActivity;
+    }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn0:
+               this.miActivity.agregarNumero("0");
+                break;
+            case R.id.btn1:
+                this.miActivity.agregarNumero("1");
+                break;
+            case R.id.btn2:
+                this.miActivity.agregarNumero("2");
+                break;
+            case R.id.btn3:
+                this.miActivity.agregarNumero("3");
+                break;
+            case R.id.btn4:
+                this.miActivity.agregarNumero("4");
+                break;
+            case R.id.btn5:
+                this.miActivity.agregarNumero("5");
+                break;
+            case R.id.btn6:
+                this.miActivity.agregarNumero("6");
+                break;
+            case R.id.btn7:
+                this.miActivity.agregarNumero("7");
+                break;
+            case R.id.btn8:
+                this.miActivity.agregarNumero("8");
+                break;
+            case R.id.btn9:
+                this.miActivity.agregarNumero("9");
+                break;
+            case R.id.btnComa:
+                //this.miActivity.agregarNumero(".");
+                break;
+            case R.id.btnMas:
+                this.miActivity.agregarOperacion("+");
 
+                break;
+            case R.id.btnMenos:
+                this.miActivity.agregarOperacion("-");
+
+                break;
+            case R.id.btnMultiplicar:
+                this.miActivity.agregarOperacion("*");
+
+                break;
+            case R.id.btnIgual:
+                this.miActivity.procesar();
+                break;
+            case R.id.btnCE:
+                this.miActivity.limpiar();
+                break;
+        }
     }
 }
